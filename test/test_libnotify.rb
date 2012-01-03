68d6a42516a66ca04321c3b78ae6666425f886c9
Peter Suschlik <ps@neopoly.de>
2012-01-03 12:43:04 +0100

    Remove delegation test.

--- test/test_libnotify.rb
+++ test/test_libnotify.rb
@@ -6,20 +6,6 @@ class TestLibnotify < MiniTest::Unit::TestCase
     assert_respond_to Libnotify, :show
   end
 
-  def test_delegation
-    skip "test delegation using mock"
-
-    # old code
-    asserts("#new calls API#new") do
-      mock(Libnotify::API).new(hash_including(:body => "test")) { true }
-      Libnotify.new(:body => "test")
-    end
-    asserts("#show calls API#show") do
-      mock(Libnotify::API).show(hash_including(:body => "test")) { true }
-      Libnotify.show(:body => "test")
-    end
-  end
-
   def test_version
     assert defined?(Libnotify::VERSION), "version is defined"
     assert Libnotify::VERSION
